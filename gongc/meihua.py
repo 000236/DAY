453b04558da2da87f3e17564aaf12b9c44fb32a2
000236 <57035824+000236@users.noreply.github.com>
2023-05-24 15:10:06 +0800

    Add files via upload

---
+++ gongc/meihua.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import time
+from PIL import Image
+
+st.markdown(
+    """
+    ## $$O_2O$$优惠券预测作业系统
+    """
+)
+
+st.markdown(
+    """
+    ### 提交作业
+    """
+)
+
+now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+df = pd.read_csv('./gongc/sc.csv')
+data = pd.read_csv('./gongc/111.csv')
+label_t = df['label']
+# 创建file_uploader组件
+uploaded_file = st.file_uploader("请提交仅含[label]列的csv文件，命名需使用自己的姓名", type={"csv", "txt"})
+if uploaded_file is not None:
+    try:
+        spectra = pd.read_csv(uploaded_file)
+        label_p = spectra['label']
+        sc = sum(label_t == label_p) / len(label_t)
+        name = uploaded_file.name.split('.')[0]
+        # st.write(name)
+        st.write('准确度为：',sc)
+        data1 = {'name': [name], 'score': [sc], 's_time': [now]}
+        data1 = pd.DataFrame(data1)
+        data = pd.concat([data, data1])
+        data = data.sort_values(by=['score', 's_time'], ascending=[False, True])
+        data = data.reset_index(drop=True)
+        # st.write(data)
+        data.to_csv('./gongc/111.csv', index=False)
+    except:
+        st.caption(':red[导入文件不规范，请按照下面的例子再进行上传文件]')
+        image = Image.open('./gongc/ces.jpg')
+        st.image(image, caption='仅有一列label的csv文件')
+
+    with st.expander("查看排行榜"):
+        data['rank'] = range(1, len(data) + 1)
+        st.write(data)
